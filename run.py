85b85f0caaead071282b786be84cd8c2ffe8ef21
margaret mwangi <margaret13mwangi@gmail.com>
2020-04-19 12:24:45 +0300

    updated run.py

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3.7
+from user import Users
+
+def create_user(uname,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = Users(uname,password)
+    return new_user
+
+def save_users(user):
+    '''
+    Fuunction to save users
+    '''
+    user.save_user()
+
+def generate_password():
+    '''
+    Function that generates a password
+    '''
